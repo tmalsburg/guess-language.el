e28742b35678eb4bdcbcbe5a5eb37021891f3b71
Titus von der Malsburg <malsburg@posteo.de>
2017-02-02 11:05:39 +0100

    Do not mess with flyspell defcustoms.

--- guess-language.el
+++ guess-language.el
@@ -125,14 +125,11 @@ current paragraph."
       (guess-language-autoset)
       (unless (string= old-dictionary ispell-local-dictionary)
         (remove-hook 'flyspell-incorrect-hook #'guess-language-autoset-and-spellcheck-maybe)
-        (flyspell-region (save-excursion (backward-paragraph) (point))
-                         (save-excursion (forward-paragraph) (point)))
-        (add-hook 'flyspell-incorrect-hook #'guess-language-autoset-and-spellcheck-maybe)))))
-
-(setq flyspell-issue-welcome-flag nil)
-(setq flyspell-issue-message-flag nil)
-
-(make-variable-buffer-local 'flyspell-incorrect-hook)
+        (let ((flyspell-issue-welcome-flag nil)
+              (flyspell-issue-message-flag nil))
+          (flyspell-region (save-excursion (backward-paragraph) (point))
+                           (save-excursion (forward-paragraph) (point)))
+          (add-hook 'flyspell-incorrect-hook #'guess-language-autoset-and-spellcheck-maybe))))))
 
 (define-minor-mode guess-language-mode
   "Toggle guess-language mode.
