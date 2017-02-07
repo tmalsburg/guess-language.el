85cfda48487483e3fb217d04bd854006e944189a
Titus von der Malsburg <malsburg@posteo.de>
2017-02-07 15:38:07 +0100

    Detect basedir in a way that’s compatible with old Emacs.

--- guess-language.el
+++ guess-language.el
@@ -113,7 +113,7 @@ enter `nil'."
   "Load language statistics."
   (cl-loop
    for lang in guess-language-languages
-   for basedir = (file-name-directory (cdr (find-function-library 'guess-language-mode)))
+   for basedir = (file-name-directory (find-library-name "guess-language"))
    for fname = (let ((dir1 (expand-file-name (symbol-name lang) basedir))
                      (dir2 (expand-file-name (symbol-name lang) (expand-file-name "trigrams" basedir))))
                  (if (file-exists-p dir1) dir1 dir2))
