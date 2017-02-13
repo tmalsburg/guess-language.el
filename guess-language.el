c0a9cd33d8233e2e0cd62b28fdb7128945b3de99
Titus von der Malsburg <malsburg@posteo.de>
2017-02-13 12:30:36 +0100

    Mention custom functions in commentary.

--- guess-language.el
+++ guess-language.el
@@ -26,10 +26,15 @@
 ;; incorrect word and changes Ispell's dictionary and typo-mode (if
 ;; present) accordingly.  If the language settings change, flyspell is
 ;; rerun but only on the current paragraph.  Guess-language thus
-;; supports documents using multiple languages.  If the paragraph is
-;; shorter than some user-defined value, none of the above happens
-;; because there is likely not enough text to guess the language
-;; correctly.
+;; supports documents using multiple languages.
+;;
+;; If the paragraph is shorter than some user-defined value, none of
+;; the above happens because there is likely not enough text to guess
+;; the language correctly.
+;;
+;; Custom functions can be triggered when a new language is detected
+;; such that users can do things like changing the input method when
+;; needed.
 ;;
 ;; The detection algorithm is based on counts of character
 ;; trigrams.  At this time, supported languages are Arabic, Czech,
